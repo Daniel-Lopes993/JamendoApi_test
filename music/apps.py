7753a5558e5061eeecc18b10c2c68cea1361a7c3
Daniel Lopes <136044417+Daniel-Lopes993@users.noreply.github.com>
2025-06-05 09:33:31 -0300

    Comentando apps.py

--- music/apps.py
+++ music/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
 
-
+# Classe de configuração da aplicação "music"
 class MusicConfig(AppConfig):
+    # Define o tipo padrão para os campos auto-incrementados (Primary Key) nas models desta app
     default_auto_field = 'django.db.models.BigAutoField'
+    
+    # Nome da aplicação (deve corresponder ao nome da pasta do app)
     name = 'music'
+
